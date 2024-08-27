bdb84c399857e894a316a6647d5697c593bf37d0
Danil <daniil-vn@mail.ru>
2024-08-28 00:34:46 +0500

    Добавьте файлы проекта.

---
+++ FirstApplication.ConsoleApp/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApplication.ConsoleHelper
+{
+    public class Helper
+    {
+        public static void Help()
+        {
+            Console.WriteLine("Помогаю");
+        }
+    }
+}
---
+++ FirstApplication.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FirstApplication.ConsoleHelper;
+
+namespace FirstApplicetion.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Даниил";
+            Console.WriteLine(MyName);
+        }
+    }
+}
