9bc0dd391b09a02f7b81a03c376eada288ed1b26
Danil <daniil-vn@mail.ru>
2024-09-07 21:19:40 +0500

    Программа задания 4.1.17. Юнит 4, добавил условия, чтобы менялся цвет фона консоли и цвет шрифта в зависимости от введённого цвета.

--- FirstApplication.ConsoleApp/Program.cs
+++ FirstApplication.ConsoleApp/Program.cs
@@ -7,18 +7,33 @@ namespace FirstApplicetion.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your name: ");
-            string name = Console.ReadLine();
-            Console.Write("Enter your age: ");
-            byte age = (byte) int.Parse(Console.ReadLine());
-            Console.WriteLine("Your name is {0} and age is {1}", name, age);
-            Console.Write("What os your favorite day of week?");
-            DayOfWeek day = (DayOfWeek) int.Parse(Console.ReadLine());
-            Console.WriteLine("Your favorite day is {0}", day);
-            Console.Write("Enter your birthdate: ");
-            var birthdate = Console.ReadLine();
-            Console.WriteLine("Your birthdate is {0}", birthdate);
-            Console.ReadKey();
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+            var color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+            }
+
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+            }
         }
     }
 }
+
